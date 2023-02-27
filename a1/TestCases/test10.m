47259990af51745b1b1862cf976f4a3b291358b5
Zhixin Orange <zhixinchen@link.cuhk.edu.cn>
2023-02-27 14:30:06 +0800

    Day 6: add more testcases

---
+++ a1/TestCases/test10.m
@@ -0,0 +1,16 @@
+-- Expected Output: 108
+begin
+    A := 1;
+    B := 2;
+    C := 3;
+    D := 4;
+    E := 5;
+    F := 6;
+    G := 7;
+    H := B+A+B + (A+B+A + (A+B+C + (A+C+D + (B+C+E + (D+E+F+(D+D+B+(D+D+B+(D+D+B+(D+D+B+(D+D+B+(D+D+B)))))))))));
+    I := B+A+B + (A+B+A + (A+B+C + (A+C+D + (B+C+E + (D+E+F+(D+D+B+(D+D+B+(D+D+B+(D+D+B+(D+D+B+(D+D+B)))))))))));
+    J := B+A+B + (A+B+A + (A+B+C + (A+C+D + (B+C+E + (D+E+F+(D+D+B+(D+D+B+(D+D+B+(D+D+B+(D+D+B+(D+D+B)))))))))));
+    K := B+A+B + (A+B+A + (A+B+C + (A+C+D + (B+C+E + (D+E+F+(D+D+B+(D+D+B+(D+D+B+(D+D+B+(D+D+B+(D+D+B)))))))))));
+    L := B+A+B + (A+B+A + (A+B+C + (A+C+D + (B+C+E + (D+E+F+(D+D+B+(D+D+B+(D+D+B+(D+D+B+(D+D+B+(D+D+B)))))))))));
+    write(L);
+end
